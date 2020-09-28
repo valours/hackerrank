aab7d216c3a855cf42e1d5354cffe22d953166e6
valours <leflem.valentin@gmail.com>
2020-09-28 23:36:14 +0200

    delete sock merchant

--- js/sockMerchant.js
+++
@@ -1,9 +0,0 @@
-module.exports = (n, ar) => {
-  let count = 0;
-  const diff = Array.from(new Set(ar));
-  diff.forEach(sock => {
-    const socks = ar.filter(s => s === sock);
-    count += (socks.length - socks.length % 2) / 2;
-  })
-  return count
-}
