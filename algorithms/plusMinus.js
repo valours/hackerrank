6a527ef3416f3ca554d07a0a78181865cd58669b
valours <leflem.valentin@gmail.com>
2022-03-12 14:43:58 +0100

    plusMinus: fixes

--- algorithms/plusMinus.js
+++ algorithms/plusMinus.js
@@ -1,7 +1,7 @@
 module.exports = (input) => {
-  const positiveNumber = input.filter(n => n > 0);
-  const negativeNumber = input.filter(n => n < 0);
-  const zero = input.filter(n => n === 0);
+  const positiveNumber = input.filter((n) => n > 0);
+  const negativeNumber = input.filter((n) => n < 0);
+  const zero = input.filter((n) => n === 0);
   const il = input.length;
   const output = [
     parseFloat((1 / il) * positiveNumber.length).toFixed(6),
--- algorithms/plusMinus.test.js
+++ algorithms/plusMinus.test.js
@@ -1,12 +1,12 @@
-import test from 'ava';
+const plusMinus = require("./plusMinus");
 
-const plus_minus = require('./plusMinus');
-
-test('plus_minus', (t) => {
+it("plusMinus", (t) => {
+  // given
   const input = [-4, 3, -9, 0, 4, 1];
-  const output = [
-    '0.500000',
-    '0.333333',
-    '0.166667'];
-  t.deepEqual(plus_minus(input), output);
+
+  // when
+  const result = plusMinus(input);
+
+  // then
+  expect(result).toEqual(["0.500000", "0.333333", "0.166667"]);
 });
