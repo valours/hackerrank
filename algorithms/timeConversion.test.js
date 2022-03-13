86a03591bf1583bea3cd1c3e479f9641384cb86c
valours <leflem.valentin@gmail.com>
2022-03-13 20:41:14 +0100

    timeConversation: replacing ava with jest

--- algorithms/timeConversion.test.js
+++ algorithms/timeConversion.test.js
@@ -1,21 +1,37 @@
-import t from 'ava';
+const timeConversion = require("./timeConversion");
 
-const timeConversion = require('./timeConversion');
+it("Il est 19:05:45", () => {
+  // given
+  const input = "07:05:45PM";
 
-t('Il est 19:05:45', (t) => {
-  const input = '07:05:45PM';
-  const output = '19:05:45';
-  t.is(timeConversion(input), output);
+  // when
+  const result = timeConversion(input);
+
+  // then
+  const output = "19:05:45";
+  expect(result).toEqual(output);
 });
 
-t('Il est 19:05:45', (t) => {
-  const input = '11:05:45PM';
-  const output = '23:05:45';
-  t.is(timeConversion(input), output);
+it("Il est 19:05:45", () => {
+  // given
+  const input = "11:05:45PM";
+
+  // when
+  const result = timeConversion(input);
+
+  // then
+  const output = "23:05:45";
+  expect(result).toEqual(output);
 });
 
-t('Il est 19:05:45', (t) => {
-  const input = '12:05:45AM';
-  const output = '00:05:45';
-  t.is(timeConversion(input), output);
+it("Il est 19:05:45", () => {
+  // given
+  const input = "12:05:45AM";
+
+  // when
+  const result = timeConversion(input);
+
+  // then
+  const output = "00:05:45";
+  expect(result).toEqual(output);
 });
