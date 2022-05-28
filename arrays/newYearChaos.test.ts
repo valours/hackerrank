73cf4032641e564fa98812980c897636e23a662e
valours <leflem.valentin@gmail.com>
2022-05-28 13:06:06 +0200

    [WIP]: newYearChaos

--- arrays/newYearChaos.test.ts
+++ arrays/newYearChaos.test.ts
@@ -1,12 +1,26 @@
 import minimumBribes from "./newYearChaos";
 
-test("new year chaos", () => {
-  // given
-  const queue = [2, 1, 5, 3, 4];
+// TODO: clean that tests
+describe("New Year Chaos", () => {
+  it("damn", () => {
+    // given
+    const queue = [2, 1, 5, 3, 4];
 
-  // when
-  const result = minimumBribes(queue);
+    // when
+    const result = minimumBribes(queue);
 
-  // then
-  expect(result).toEqual(3);
+    // then
+    expect(result).toEqual(3);
+  });
+
+  it("damn", () => {
+    // given
+    const queue = [2, 5, 1, 3, 4];
+
+    // when
+    const result = minimumBribes(queue);
+
+    // then
+    expect(result).toEqual(7);
+  });
 });
