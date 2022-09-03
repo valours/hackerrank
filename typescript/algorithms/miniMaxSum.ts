2728ddba537c1e45e6c808970ff757bd9cfe8595
valours <leflem.valentin@gmail.com>
2022-09-04 01:05:35 +0200

    :refactor: refact miniMaxSum

---
+++ typescript/algorithms/miniMaxSum.ts
@@ -0,0 +1,20 @@
+const miniMaxSum = (arr: any[]) => {
+  const arrayStored = arr.sort();
+
+  const subMin = [...arrayStored];
+  subMin.pop();
+
+  const subMax = [...arrayStored];
+  subMax.shift();
+
+  const add = (accumulator: number, currentValue: number) =>
+    accumulator + currentValue;
+
+  const result = [subMin.reduce(add), subMax.reduce(add)].join(" ");
+
+  console.log(result);
+
+  return result;
+};
+
+export default miniMaxSum;
