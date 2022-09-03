b1607f2430e2a986ee7b5df30d7252f1a756fd3e
valours <leflem.valentin@gmail.com>
2022-09-04 00:40:26 +0200

    :recycle: refact countingValley

---
+++ typescript/algorithms/countingValley.ts
@@ -0,0 +1,16 @@
+const countingValleys = (steps, path) => {
+  const pathSplited = path.split("");
+  let countValley = 0;
+  pathSplited.reduce((previousElevation, p) => {
+    const elevation = p === "U" ? previousElevation + 1 : previousElevation - 1;
+
+    if (elevation === 0 && previousElevation < elevation) {
+      ++countValley;
+    }
+    return elevation;
+  }, 0);
+
+  return countValley;
+};
+
+export default countingValleys;
